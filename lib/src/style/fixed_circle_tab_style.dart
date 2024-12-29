17ede40b52b9ce04907844e573efa94363007179
Halima <halima@MacBook-Pro-Halima.local>
2024-12-29 19:32:45 +0200

    chore: Update fixed circle tab style

--- lib/src/style/fixed_circle_tab_style.dart
+++ lib/src/style/fixed_circle_tab_style.dart
@@ -44,49 +44,57 @@ class FixedCircleTabStyle extends InnerBuilder {
       : super(items: items, activeColor: activeColor, color: color);
 
   @override
-  Widget build(BuildContext context, int index, bool active) {
-    var c = active ? activeColor : color;
-    var item = items[index];
-    var style = ofStyle(context);
-    var textStyle = style.textStyle(c, item.fontFamily);
-    var margin = style.activeIconMargin;
+Widget build(BuildContext context, int index, bool active) {
+  var c = active ? activeColor : color;
+  var item = items[index];
+  var style = ofStyle(context);
+  var textStyle = style.textStyle(c, item.fontFamily);
+  var margin = style.activeIconMargin;
 
-    if (index == convexIndex) {
-      final item = items[index];
-      return Container(
-        // necessary otherwise the badge will not large enough
-        width: style.layoutSize,
-        height: style.layoutSize,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: fixedColor,
-        ),
-       margin: EdgeInsets.all(margin), 
-        child: BlendImageIcon(
-          active ? item.activeIcon ?? item.icon : item.icon,
-          size: style.activeIconSize,
-          color: item.blend ? backgroundColor : null,
-        ),
-      );
-    }
-
-    var noLabel = style.hideEmptyLabel && hasNoText(item);
-    var icon = BlendImageIcon(
-      active ? item.activeIcon ?? item.icon : item.icon,
-      color: item.blend ? (c) : null,
-      size: style.iconSize,
-    );
-    var children = noLabel
-        ? <Widget>[icon]
-        : <Widget>[icon, SizedBox(height: 5.0), Text(item.title ?? '', style: textStyle)];
+  if (index == convexIndex) {
+    final item = items[index];
     return Container(
-      padding: EdgeInsets.only(bottom: 2),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: children,
+      // necessary otherwise the badge will not large enough
+      width: style.layoutSize,
+      height: style.layoutSize,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: fixedColor,
+      ),
+      margin: EdgeInsets.all(margin),
+      child: BlendImageIcon(
+        active ? item.activeIcon ?? item.icon : item.icon,
+        size: style.activeIconSize,
+        color: item.blend ? backgroundColor : null,
       ),
     );
   }
+
+  var noLabel = style.hideEmptyLabel && hasNoText(item);
+  var icon = BlendImageIcon(
+    active ? item.activeIcon ?? item.icon : item.icon,
+    color: item.blend ? (c) : null,
+    size: style.iconSize,
+  );
+  var children = noLabel
+      ? <Widget>[icon]
+      : <Widget>[
+          Flexible(
+            child: icon,
+          ),
+          const SizedBox(height: 5.0),
+          Flexible(
+            child: Text(item.title ?? '', style: textStyle),
+          ),
+        ];
+
+  return Container(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: children,
+    ),
+  );
+}
 
   @override
   bool fixed() {
